9f4a5b77f1b63cf5e530b2e08d876a51927a1841
Parth Thakur <46916990+hbot07@users.noreply.github.com>
2020-02-29 20:38:59 +0530

    Added PrivateInfo class with empty variables

---
+++ src/main/java/PrivateInfo.java
@@ -0,0 +1,14 @@
+
+public class PrivateInfo {
+    //This class was created to protect private information while the project code goes to github
+    // To protect ACCOUNT_SID and AUTH_TOKEN
+    
+    public static final String ACCOUNT_SID ="";
+
+    public static final String AUTH_TOKEN ="";
+
+    public static String to="";
+
+    public static String from="";
+
+}
