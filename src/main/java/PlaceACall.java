82bd29eb307730309ac7bb39dfd71e23baf74822
Parth <hbot07@gmail.com>
2020-02-29 22:25:34 +0530

    Added exception handling

--- src/main/java/PlaceACall.java
+++ src/main/java/PlaceACall.java
@@ -13,6 +13,7 @@ Scanner sc=new Scanner(System.in);
             PrivateInfo.AUTH_TOKEN;
 
     public static void main() {
+        try{
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Call call = Call.creator(
                 new com.twilio.type.PhoneNumber(PrivateInfo.to),
@@ -20,7 +21,11 @@ Scanner sc=new Scanner(System.in);
                 URI.create("https://parththakur.000webhostapp.com/response.xml"))//this is twiml which is executed when call is accepted
                 .create();
 
-        System.out.println(call.getSid());//If you wonder what the call would sound like - it would just drop in a
+        System.out.println("Call successfully placed with ID: "+call.getSid());//If you wonder what the call would sound like - it would just drop in a
                                           //hello , play a song ,and say bye .
+    }catch(Exception e){
+            System.out.println("Did you add your information to PrivateInfo.java correctly ?");
+        }
+
     }
 }
